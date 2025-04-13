0cc7e41db8088c3e64096dd3455dfc7b84784103
Zhihui Huang <zhh399@mail.usask.ca>
2025-04-13 15:46:05 -0600

    Create a new mongo db project and a new clustern and set up the mongo db connection string in .evn file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 
+// In order to access the variable in .env file, 
+// we need to call dotenv.config() function first
+dotenv.config();
 const app = express();
+
+// we need to use process.env.varible_name to 
+// access the variable in .env file
+const PORT = process.env.PORT ;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-    console.log("Server is starting now")
+app.listen(PORT, () => {
+    console.log("Server is starting on port: " + PORT)
 });
