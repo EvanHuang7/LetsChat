75126d5b40d405832fa45ed4ceec7c8a90f1c50c
Zhihui Huang <zhh399@mail.usask.ca>
2025-04-13 19:22:10 -0600

    add  generateToken function to generate JWT token and sent it to user when a user singup or login

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken"
+
+export const generateToken = (userId, res) => {
+    // We use unique userId as payload to create the JWT token
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+    })
+
+    // Send the generated JWT token to user's cookie via response cookie
+    res.cookie("jwt", token, {
+        // This JWT token will stay in user's cookie for 7 days.
+        // After 7 days, user needs to login again to get a new JWT.
+        maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+        // Make sure this token is not accessable via JavaScript
+        // Prevent XSS attacks, cross-site scripting attacks
+        httpOnly: true, 
+        // Prevent CSRF attacks, cross-site request forgery attacks
+        sameSite: "strict", 
+        // determin if it is https or http. https is secure for real production
+        // http is not secure for localHost.
+        // This field should be True for real production case
+        secure: process.env.NODE_ENV !== "development"
+    })
+
+    return token
+}
