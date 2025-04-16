c9590be7d081f7fa654b93e4b63bf42dca8a1f1b
Zhihui Huang <zhh399@mail.usask.ca>
2025-04-16 16:51:14 -0600

    add checkAuth() function in useAuthStore and call it when app starts

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,27 @@
 import { create } from "zustand"
+import { axiosInstance } from "../lib/axios.js"
 
 export const useAuthStore = create((set) =>({
     authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+
     isCheckingAuth: true,
+
+    checkAuth: async() => {
+        try {
+            // Call the auth check endpoint
+            const res = await axiosInstance.get("/auth/check")
+
+            set({authUser: res.data})
+        } catch (error) {
+            console.log("Error in checkAuth: ", error)
+            // When api call returns errors, that means 
+            // user is not authenticated
+            set({authUser: null})
+        } finally {
+            set({isCheckingAuth: false})
+        }
+    }
 }))
