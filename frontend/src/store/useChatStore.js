52037863cbf3a2817a76a3f66429098dc29be0c1
Zhihui Huang <zhh399@mail.usask.ca>
2025-04-19 16:27:08 -0600

    add sendMessage api call function to useChatStore

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -5,7 +5,7 @@ import { axiosInstance } from "../lib/axios.js"
 
 // Zustand is a handy state management tool for 
 // managing state in React apps
-export const useChatStore = create((set) =>({
+export const useChatStore = create((set, get) =>({
     users: [],
     selectedUser: null,
     messages: [],
@@ -42,11 +42,24 @@ export const useChatStore = create((set) =>({
             set({isMessagesLoading: false})
         }
     },
+
+    // Function to make HTTP call to "api/message/send/:id" endpoint
+    sendMessage: async(data) => {
+        try {
+            const {selectedUser, messages} = get()
+            // Call the send message endpoint
+            const res = await axiosInstance.post(`/message/send/${selectedUser._id}`, data)
+            // Add new sent message to messages list
+            set({messages: [...messages, res.data]})
+        } catch (error) {
+            console.log("Error in sendMessage: ", error)
+            toast.error(error.response.data.message)
+        } 
+    },
+
     //TODO: optimize this one latter
     setSelectedUser: (selectedUser) => {
         set({ selectedUser })
     },
-
-
 }))
 
