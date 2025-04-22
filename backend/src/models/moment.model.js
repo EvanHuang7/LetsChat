5114ce1fb8a8b8b47ff175181c19a5d13e0bc8e1
Zhihui Huang <zhh399@mail.usask.ca>
2025-04-21 18:04:22 -0600

    add moment model

---
+++ backend/src/models/moment.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const momentSchema = new mongoose.Schema(
+  {
+    posterId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  // It will create all timestamps fields, such as createdAt, UpdatedAt
+  { timestamps: true }
+);
+
+// Mongo will use "moments" as table name
+const Moment = mongoose.model("Moment", momentSchema);
+
+export default Moment;
