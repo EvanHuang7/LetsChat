76e229ab503a13d101075a4da104947646ae3196
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-01 00:51:34 -0600

    add emitNewAcceptedFriendEventService and emitNewGroupMemberEventService

--- backend/src/services/socket.service.js
+++ backend/src/services/socket.service.js
@@ -1,8 +1,11 @@
 import { io, getSocketIdByUserId } from "../lib/socket.js";
 
-// The service function to emit new message event
+// The service function to emit new message event to all oneline users
+// of conversation users list
 export const emitNewMessageEventService = async ({
+  // it's a populated objects array when passed in
   userIds,
+  // it's a id string when passed in
   senderId,
   newMessage,
 }) => {
@@ -45,6 +48,103 @@ export const emitNewMessageEventService = async ({
     return {
       error:
         error.message || "An error occurred while emiting new message event",
+    };
+  }
+};
+
+// The service function to emit new accepted friend event to connection
+// sender if he/she is online
+// No need to emit to connection receiver because he/she has to
+// change the page with back-end data loaded to view new conversation.
+export const emitNewAcceptedFriendEventService = async ({
+  // it's a id string when passed in
+  connectionSenderId,
+  newConvoInfoOfUser,
+}) => {
+  try {
+    // Validate if the input
+    if (!connectionSenderId) {
+      return {
+        error: "ConnectionSenderId is required",
+      };
+    }
+
+    if (!newConvoInfoOfUser) {
+      return {
+        error: "newConvoInfoOfUser is required",
+      };
+    }
+
+    const connectionSenderSocketId = getSocketIdByUserId(connectionSenderId);
+    if (connectionSenderSocketId) {
+      io.to(connectionSenderSocketId).emit(
+        "newAcceptedFriend",
+        newConvoInfoOfUser
+      );
+    }
+
+    return { error: null };
+  } catch (error) {
+    return {
+      error:
+        error.message ||
+        "An error occurred while emiting new accepted friend event",
+    };
+  }
+};
+
+// The service function to emit new group member event to all existing
+// online group members
+export const emitNewGroupMemberEventService = async ({
+  // it's a populated objects array when passed in
+  userIds,
+  // it's a id string when passed in
+  newGroupMemberId,
+  newConvoInfoOfUser,
+}) => {
+  try {
+    // Validate if the input
+    if (userIds.length < 1) {
+      return {
+        error: "UserIds is required",
+      };
+    }
+
+    if (!newGroupMemberId) {
+      return {
+        error: "newGroupMemberId is required",
+      };
+    }
+
+    if (!newConvoInfoOfUser) {
+      return {
+        error: "newConvoInfoOfUser is required",
+      };
+    }
+
+    // Get all users except new group member and emit newGroupMember event
+    // to all their sockets if online
+    const promises = userIds
+      .filter((user) => user._id.toString() !== newGroupMemberId.toString())
+      .map((user) => {
+        const receiverSocketId = getSocketIdByUserId(user._id);
+        if (receiverSocketId) {
+          return io
+            .to(receiverSocketId)
+            .emit("newGroupMember", newConvoInfoOfUser);
+        }
+      });
+
+    // Emit events at once
+    await Promise.all(promises);
+
+    return { error: null };
+  } catch (error) {
+    // If an error occurs, return the error message
+    return {
+      error:
+        error.message ||
+        "An error occurred while emiting new group member event",
     };
   }
 };
